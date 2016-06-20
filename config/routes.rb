e7f170d11648c53dd1d37558ef54ddf0282c00e8
whatthemandy <amcdorman@gmail.com>
2016-06-19 17:35:56 -0700

    initial setup/commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Blogger::Application.routes.draw do
+  resources :articles
+end
+
+# rake routes:
+#       Prefix   Verb     URI Pattern                    Controller#Action
+#     articles   GET      /articles(.:format)            articles#index
+#                POST     /articles(.:format)            articles#create
+#  new_article   GET      /articles/new(.:format)        articles#new
+# edit_article   GET      /articles/:id/edit(.:format)   articles#edit
+#      article   GET      /articles/:id(.:format)        articles#show
+#                PATCH    /articles/:id(.:format)        articles#update
+#                PUT      /articles/:id(.:format)        articles#update
+#                DELETE   /articles/:id(.:format)        articles#destroy
