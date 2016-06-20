98002613e5726707c4bcc97cfa7733a48eb037b2
whatthemandy <amcdorman@gmail.com>
2016-06-19 18:07:25 -0700

    work on show method, create helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
-class ArticlesController < ApplicationController
+ include ArticlesHelper
+
+ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
@@ -6,6 +8,19 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # @article = Article.new
+    # @article.title = params[:article][:title]
+    # @article.body = params[:article][:body]
+    @article = Article.new(article_params)  # added method to helper file
+    @article.save
+    redirect_to article_path(@article)
   end
 
 end
