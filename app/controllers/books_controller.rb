0d272f56b6014e1d3b9666e92e988da649c56245
Mandy McDorman <email": null>
2016-06-26 14:59:20 -0700

    add form and routes for creating new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,4 +13,22 @@ class BooksController < ApplicationController
     # @comments = @book.comments.order('created_at DESC').paginate :page => params[:page], :per_page => 5
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "#{@book.title} saved."
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :thoughts)
+  end
 end
