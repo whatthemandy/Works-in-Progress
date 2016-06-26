e8d6e41bf4e8fb230fb1fb5c9447a9d06f5f0a8e
Mandy McDorman <email": null>
2016-06-25 22:21:05 -0700

    update routes in comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,28 @@ Blogger::Application.routes.draw do
   root to: 'articles#index'  # makes '/' show our articles index page
 
   resources :articles do
-    resources :comments  # specify comments as a sub-resource
+    resources :comments  # specify comments as a sub-resource (nested inside articles)
   end
 
 end
 
+
 # rake routes:
-#       Prefix   Verb     URI Pattern                    Controller#Action
-#     articles   GET      /articles(.:format)            articles#index
-#                POST     /articles(.:format)            articles#create
-#  new_article   GET      /articles/new(.:format)        articles#new
-# edit_article   GET      /articles/:id/edit(.:format)   articles#edit
-#      article   GET      /articles/:id(.:format)        articles#show
-#                PATCH    /articles/:id(.:format)        articles#update
-#                PUT      /articles/:id(.:format)        articles#update
-#                DELETE   /articles/:id(.:format)        articles#destroy
+#               Prefix Verb   URI Pattern                                       Controller#Action
+#                 root GET    /                                                 articles#index
+#     article_comments GET    /articles/:article_id/comments(.:format)          comments#index
+#                      POST   /articles/:article_id/comments(.:format)          comments#create
+#  new_article_comment GET    /articles/:article_id/comments/new(.:format)      comments#new
+# edit_article_comment GET    /articles/:article_id/comments/:id/edit(.:format) comments#edit
+#      article_comment GET    /articles/:article_id/comments/:id(.:format)      comments#show
+#                      PATCH  /articles/:article_id/comments/:id(.:format)      comments#update
+#                      PUT    /articles/:article_id/comments/:id(.:format)      comments#update
+#                      DELETE /articles/:article_id/comments/:id(.:format)      comments#destroy
+#             articles GET    /articles(.:format)                               articles#index
+#                      POST   /articles(.:format)                               articles#create
+#          new_article GET    /articles/new(.:format)                           articles#new
+#         edit_article GET    /articles/:id/edit(.:format)                      articles#edit
+#              article GET    /articles/:id(.:format)                           articles#show
+#                      PATCH  /articles/:id(.:format)                           articles#update
+#                      PUT    /articles/:id(.:format)                           articles#update
+#                      DELETE /articles/:id(.:format)                           articles#destroy
