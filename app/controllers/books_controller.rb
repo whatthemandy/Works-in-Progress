7f2215253ae563715ab7878f78c053b34215286d
Mandy McDorman <email": null>
2016-06-26 15:30:47 -0700

    add links and routes for editing and deleting books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,27 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def update
+    @book = Book.find params[:id]
+    if @book.update_attributes(book_params)
+      flash[:notice] = "#{@book.title} saved."
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    flash[:notice] = "#{book.title} deleted."
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :thoughts)
