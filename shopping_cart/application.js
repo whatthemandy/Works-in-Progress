74c1180e81aac0953a1a4461165b853771f4e67b
Mandy McDorman <email": null>
2016-06-25 19:05:35 -0700

    add cart items/quantity update

--- shopping_cart/application.js
+++ shopping_cart/application.js
@@ -1,9 +1,12 @@
 $(function(){
-
   var inventory = $(rawInventory); // from data.js
   var prototypeItem = $('#prototype-item');
   prototypeItem.detach();
 
+  var prototypeCartItem = $('#prototype-cart');
+  prototypeCartItem.detach();
+
+  // add each item from data file to inventory list
   inventory.each(function(){
     var item = prototypeItem.clone();
     item.find('h3').text(this.name);
@@ -11,6 +14,21 @@ $(function(){
     item.find('span.qty').text(this.stock);
     item.attr("id", "product_" + this.product_id);
     $('#inventory').append(item);
-  });
 
+    // add each item from inventory list to cart list
+    var cartItem = prototypeCartItem.clone();
+    cartItem.find('h3').text(this.name);
+    cartItem.attr("id", "product_" + this.product_id);
+    cartItem.find('span.price').text(this.price)
+    $('#cart').append(cartItem);
+
+    // on "add to cart" click, add items to cart & update quantities
+    item.on('click', function () {
+      targetId = $(this).attr('id');
+      var target = $('div#cart div#' + targetId);
+      var quantity = target.find('span.qty');
+      var current = Number(quantity.text());
+      quantity.text(current += 1);
+    });
+  });
 });
